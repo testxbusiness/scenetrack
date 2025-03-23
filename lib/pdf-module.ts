580966788cdb9a8dfc0508981ffe794424c730bf
Famiglia Politi <famigliapoliti@Air-di-Famiglia.lan>
2025-03-23 14:52:18 +0100

    Corretto errore nel modulo PDF.js

--- lib/pdf-module.ts
+++ lib/pdf-module.ts
@@ -4,6 +4,14 @@
  * risolvendo i problemi di caricamento su dispositivi mobili
  */
 import * as pdfjs from 'pdfjs-dist';
-import 'pdfjs-dist/build/pdf.worker.entry';
+
+// Imposta manualmente il worker URL
+if (typeof window !== 'undefined') {
+  const pdfjsLib = pdfjs as any;
+  if (pdfjsLib.GlobalWorkerOptions) {
+    pdfjsLib.GlobalWorkerOptions.workerSrc = new URL('/pdf.worker.min.js', window.location.origin).href;
+    console.log('PDF.js worker URL set to:', pdfjsLib.GlobalWorkerOptions.workerSrc);
+  }
+}
 
 export default pdfjs;
